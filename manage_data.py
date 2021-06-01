5c5ea47ed63d45b3709ceeea30f44770f8c4c0c9
xvrong <1187518864@qq.com>
2021-06-01 14:18:39 +0800

    增加自动管理png文件

--- manage_data.py
+++ manage_data.py
@@ -4,11 +4,12 @@
 @Time    : 2021/05/18
 @Author  : xvrong
 @File    : manage_data.py
-@Function: 整理数据文件
+@Function: 整理数据文件和画图文件
 '''
 import time
 import os
 import shutil
+import re
 
 
 def mkdir(path):
@@ -20,22 +21,34 @@ def mkdir(path):
 
 
 def main():
-    # 临时数据文件名
+    # 数据文件名
     file_path = 'data.txt'
     # 判断数据是否存在，不存在，直接退出
     if (os.path.exists(file_path)):
         # 获取时间
-        now = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
+        cur_time = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
         # 分割
-        [day, hour] = now.split(' ')
+        [cur_day, cur_time] = cur_time.split(' ')
         # 创建名为当前年月日的文件夹
-        dir_path = os.path.join('data', day)
+        dir_path = os.path.join('data', cur_day)
         mkdir(dir_path)
         # 通过当前时间为文件命名
-        new_name = hour + '.txt'
-        # 移动文件
-        file_new_path = os.path.join(dir_path, new_name)
-        shutil.move(file_path, file_new_path)
+        new_name = cur_time + '.txt'
+
+        # 移动文件数据文件
+        dest_path = os.path.join(dir_path, new_name)
+        shutil.move(file_path, dest_path)
+
+        # 移动png文件
+        file_list = os.listdir()
+        png_list = []
+        for file in file_list:
+            if(file.endswith('.png')):
+                png_list.append(file)
+        for i in range(0, len(png_list)):
+            new_name = cur_time + '-' + str(i) + '.png'
+            dest_path = os.path.join(dir_path, new_name)
+            shutil.move(png_list[i], dest_path)
 
 
 if __name__ == '__main__':
